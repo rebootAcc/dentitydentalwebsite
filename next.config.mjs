8513cbe577062423d277edb17fd4c178b4ab445f
Achinta27 <achinta27sep1999@gmail.com>
2025-11-26 15:23:29 +0530

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  distDir: "dist",
-  trailingSlash: true,
+  // reactStrictMode: true,
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
+  // distDir: "dist",
+  // trailingSlash: true,
 };
 
 export default nextConfig;
